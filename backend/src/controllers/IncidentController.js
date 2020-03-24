71ff5d6eb94cac3315789fe6779d5c36983c4c5e
Nathan Lima <nathanlima.b@gmail.com>
2020-03-24 17:33:04 -0300

    Criado o método para deletar um caso específico

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,14 +1,14 @@
 const connection = require('../database/connection')
 
 module.exports = {
-    async index(require, response){
+    async index(request, response){
         const incidents = await connection('incidents').select('*');
         return response.json( incidents );
     },
 
-    async create(require, response){
-        const { title, description, value } = require.body;
-        const ong_id = require.headers.authorization;
+    async create(request, response){
+        const { title, description, value } = request.body;
+        const ong_id = request.headers.authorization;
 
         const [id] = await connection('incidents').insert({
             title,
@@ -17,5 +17,23 @@ module.exports = {
             ong_id,
         });
         return response.json({ id });
+    },
+
+    async delete(request, response){
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incidents = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+        if (incidents.ong_id != ong_id){
+            return response.status(401).json({ erro: 'Operação não permitida.' });
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
     }
 };

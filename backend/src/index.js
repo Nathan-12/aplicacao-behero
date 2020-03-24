ba6dd240bf32249688ff75eedcc790211fc73eb7
Nathan Lima <nathanlima.b@gmail.com>
2020-03-24 18:26:59 -0300

    Criado o método delete e pagination

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
-const connection = require('./database/connection');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/session', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
@@ -11,6 +15,8 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 
 module.exports = routes;
